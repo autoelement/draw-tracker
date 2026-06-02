e4c33ff111277ddfdea9cacd8c4f2d56e031a1ab
Draw <50653689+autoelement@users.noreply.github.com>
2026-06-02 21:33:52 +0400

    Update results.py

--- results.py
+++ results.py
@@ -36,7 +36,7 @@ def check_results(matches):
         return []
 
     model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash"
+    model_name="gemini-1.5-flash"
     )
 
     match_list = "\n".join([
